c98dce56c2b9baadf90338dd6d3bd83d1ddd2363
Edouard DA SILVA <esdasilva@hotmail.fr>
2024-02-17 00:35:45 +0100

    Routage créer vers les pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <h1 className="m-4">Trouve ton artisan</h1>
-      <h2 className="test1">Test couleur</h2>
-      <h3 className="test2">test couleur</h3>
+      <Outlet/>
     </>
   );
 }
