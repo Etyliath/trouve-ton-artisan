d80172f0bc78538b599041a9a928a449abe1571a
Edouard DA SILVA <esdasilva@hotmail.fr>
2024-02-18 14:56:29 +0100

    begin create component Issue #7

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import '../assets/scss/footer.scss'
+
+export function Footer(){
+    return <div className="container-fluid bg-primary p-0" >
+        <div className="design mx-4"> 
+        </div>
+        <div>test</div>
+    </div>
+}
