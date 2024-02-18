7758d1a0b44adec265c6aa8780eea629a9690fab
Edouard DA SILVA <esdasilva@hotmail.fr>
2024-02-18 23:42:07 +0100

    commit fin de journée Issue #10

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,29 @@
-import {ArtisanCard} from '../components/ArtisanCard'
+import { ArtisanCard } from '../components/ArtisanCard';
+import datas from '../assets/data/datas.json';
 
 export function Home() {
+  const artisans = datas.artisans;
+  const filterArtisans = artisans.filter(
+    (artisan) => artisan.id === '2' || artisan.id === '4' || artisan.id === '10'
+  );
+
   return (
     <main className='m-4 d-flex justify-content-center flex-column align-items-center'>
       <h1 className=''> Trouve ton artisan</h1>
-      <div>
-        <h2>Artisans du mois</h2>
-        <ArtisanCard header={'menusier'} title={'Jean Paul'} location={'Rozay'} note={3.6} />
+      <div className=''>
+        <h2 className='mx-auto text-center '>Artisans du mois</h2>
+        <div className='d-flex'>
+          {filterArtisans.map((artisan) => (
+            <div key={artisan.id} className=''>
+              <ArtisanCard
+                header={artisan.specialty}
+                title={artisan.name}
+                location={artisan.location}
+                note={parseFloat(artisan.note)}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
