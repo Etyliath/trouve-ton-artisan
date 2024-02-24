ae5dc31834d8e06eb60f3c8fb4d128c89d7d487a
Edouard DA SILVA <esdasilva@hotmail.fr>
2024-02-24 21:49:34 +0100

    create form to send a message for a artisan Ok Issue #18

--- src/pages/SingleArtisan.jsx
+++ src/pages/SingleArtisan.jsx
@@ -6,81 +6,163 @@ import { useState } from 'react';
 export function SingleArtisan() {
   const { id } = useParams();
   const artisan = datas.artisans.find((v) => v.id === id);
-  const [visible, setVisible] = useState({
-    inputName: false,
-    inputMail: false,
-    inputSubject: false,
-    texareaMessage: false,
-  });
-
-  const [validatorMessage, setValidatorMessage] = useState('');
-
-  const regexName = /^[a-z]+?(\s?-?)[a-z]* ?[a-z]*$/gi;
-  const regexMail = /^[a-z0-9-\\.]+@[a-z0-9-]+.[a-z]{2,}$/gi;
-  const [messageToSend, setMessageToSend] = useState({
+  const emailArtisant=artisan.email
+  let messageToSend = {
     name: '',
     email: '',
     subject: '',
     message: '',
-  });
-
-  const handleChange = (e) => {
-    if (regexName.test(e.target.value) || e.target.value === '') {
-      if (e.target.id === 'name') {
-        setMessageToSend({ ...messageToSend, name: e.target.value });
-        setVisible({ ...visible, inputName: false });
-      }
-    } else {
-      setValidatorMessage(
-        'valeur incorrect seul chiffres et les lettres sont autorisé'
-      );
-      if (e.target.id === 'name') {
-        setVisible({ ...visible, inputName: true });
-      }
-    }
-    if (regexMail.test(e.target.value) || e.target.value === '') {
-      console.log(e.target.value);
-      if (e.target.id === 'email') {
-        setMessageToSend({ ...messageToSend, email: e.target.value });
-        setVisible({ ...visible, inputMail: false });
-      }
-    } else {
-      if (e.target.id === 'email') {
-        setVisible({ ...visible, inputMail: true });
-      }
-    }
-    if (regexName.test(e.target.value) || e.target.value === '') {
-      if (e.target.id === 'subject') {
-        setMessageToSend({ ...messageToSend, subject: e.target.value });
-        setVisible({ ...visible, inputSubject: false });
-      }
-    } else {
-      setValidatorMessage(
-        'valeur incorrect seul chiffres et les lettres sont autorisé'
-      );
-      if (e.target.id === 'subject') {
-        setVisible({ ...visible, inputSubject: true });
-      }
-    }
-    if (regexName.test(e.target.value) || e.target.value === '') {
-      if (e.target.id === 'message') {
-        setMessageToSend({ ...messageToSend, message: e.target.value });
-        setVisible({ ...visible, texareaMessage: false });
-      }
-    } else {
-      setValidatorMessage(
-        'valeur incorrect seul chiffres et les lettres sont autorisé'
-      );
-      if (e.target.id === 'message') {
-        setVisible({ ...visible, texareaMessage: true });
-      }
-    }
   };
 
+  // varialbes pour afficher enlever les span
+  const [spanName, setSpanName] = useState(false);
+  const [spanEmail, setSpanEmail] = useState(false);
+  const [spanSubject, setSpanSubject] = useState(false);
+  const [spanMessage, setSpanMessage] = useState(false);
+
+  //liste des regex utiliser
+  const regexList = {
+    name: /^[a-z]+?(\s?-?)[a-z]* ?[a-z]*$/gi,
+    email: /^[a-z0-9-\\.]+@[a-z0-9-]+.[a-z]{2,}$/gi,
+    subject: /^[a-z]+?(\s?-?)[a-z]* ?[a-z]*$/gi,
+    message: /^[a-z]+?(\s?-?)[a-z]* ?[a-z]*$/gi,
+  };
+
+  //variales des champ controler par React
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
+  /**
+   * controle des champ du formulaire
+   * @param {event} e
+   */
+  const handleChange = (e) => {
+    const regex = regexName(e.target.id);
+    validateRegex(e.target.value, regex, e.target.id);
+  };
+
+  /**
+   * determine ma regex à utiliser
+   * @param {string} id
+   * @returns
+   */
+  function regexName(id) {
+    if (id.toString() === 'name') {
+      return regexList.name;
+    }
+    if (id.toString() === 'email') {
+      return regexList.email;
+    }
+    if (id.toString() === 'subject') {
+      return regexList.subject;
+    }
+    if (id.toString() === 'message') {
+      return regexList.message;
+    }
+  }
+
+  /**
+   * valide l'entée des champ Input ou textarea
+   * @param {string} entry
+   * @param {RegExp} regex
+   * @param {string} id id de l'input
+   * @returns {boolean}
+   */
+  function validateRegex(value, regex, id) {
+    if (regex.test(value)) {
+      removeSpan(id, value);
+    } else if (value === '') {
+      displaySpan(id, value);
+    } else {
+      displaySpan(id, value);
+    }
+  }
+
+  /**
+   * permet d'afficher la span en dessous de l'input en cas d'erreur
+   * @param {string} id
+   * @param {string} value
+   * @returns
+   */
+  function displaySpan(id, value) {
+    if (id.toString() === 'name') {
+      setSpanName(true);
+      setName(value);
+    }
+    if (id.toString() === 'email') {
+      setSpanEmail(true);
+      setEmail(value);
+    }
+    if (id.toString() === 'subject') {
+      setSpanSubject(true);
+      setSubject(value);
+    }
+    if (id.toString() === 'message') {
+      setSpanMessage(true);
+      setMessage(value);
+    }
+  }
+
+  /**
+   * retire la span
+   * @param {string} id
+   * @param {string} value
+   */
+  function removeSpan(id, value) {
+    if (id.toString() === 'name') {
+      setSpanName(false);
+      setName(value);
+    }
+    if (id.toString() === 'email') {
+      setSpanEmail(false);
+      setEmail(value);
+    }
+    if (id.toString() === 'subject') {
+      setSpanSubject(false);
+      setSubject(value);
+    }
+    if (id.toString() === 'message') {
+      setSpanMessage(false);
+      setMessage(value);
+    }
+  }
+
+  /**
+   * Envoie les données du formulaire
+   * @param {Event} e
+   */
   const handleSubmit = (e) => {
     e.preventDefault();
-  };
+    if (
+      !spanName &&
+      !spanEmail &&
+      !spanSubject &&
+      !spanMessage &&
+      name !== '' &&
+      email !== '' &&
+      subject !== '' &&
+      message !== ''
+    ) {
+      console.log('formulaire Ok');
+      messageToSend.name = name;
+      messageToSend.email = email;
+      messageToSend.subject = subject;
+      messageToSend.message = message;
+      
 
+      reset()
+    } else {
+      console.log('Pas ok');
+    }
+  };
+  const reset = ()=>{
+     setName('')
+     setEmail('')
+     setSubject('')
+     setMessage('')
+  }
   return (
     <main>
       <h2 className=' text-center mt-4'>Contacter votre artisan</h2>
@@ -102,61 +184,67 @@ export function SingleArtisan() {
             <label htmlFor='name' className='form-label'>
               Nom
             </label>
+            <span className='ps-1 text-danger'>*</span>
             <input
               type='text'
               className='form-control'
               id='name'
-              value={messageToSend.name}
+              value={name}
               onChange={handleChange}
             />
-            {visible.inputName && (
-              <span className=' text-danger'>{validatorMessage}</span>
+            {spanName && (
+              <span className=' text-danger'>
+                Entée incorrect seul les lettres et - sont autorisées
+              </span>
             )}
           </div>
           <div className='mb-3'>
             <label htmlFor='email' className='form-label'>
               adresse mail
             </label>
+            <span className='ps-1 text-danger'>*</span>
             <input
-              type='email'
+              type='text'
               className='form-control'
               id='email'
               placeholder='name@example.com'
-              value={messageToSend.email}
+              value={email}
               onChange={handleChange}
             />
-            {visible.inputMail && (
-              <span className=' text-danger'>{validatorMessage}</span>
+            {spanEmail && (
+              <span className=' text-danger'>format email incorrect</span>
             )}
           </div>
           <div className='mb-3'>
             <label htmlFor='subject' className='form-label'>
               Sujet
             </label>
+            <span className='ps-1 text-danger'>*</span>
             <input
               type='text'
               className='form-control'
               id='subject'
-              value={messageToSend.subject}
+              value={subject}
               onChange={handleChange}
             />
-            {visible.inputSubject && (
-              <span className=' text-danger'>{validatorMessage}</span>
+            {spanSubject && (
+              <span className=' text-danger'>Entée incorrect</span>
             )}
           </div>
           <div className='mb-3'>
             <label htmlFor='message' className='form-label'>
               Message
             </label>
+            <span className='ps-1 text-danger'>*</span>
             <textarea
               className='form-control'
               id='message'
               rows='3'
-              value={messageToSend.message}
+              value={message}
               onChange={handleChange}
             ></textarea>
-            {visible.texareaMessage && (
-              <span className=' text-danger'>{validatorMessage}</span>
+            {spanMessage && (
+              <span className=' text-danger'>Entée incorrect</span>
             )}
           </div>
           <button
